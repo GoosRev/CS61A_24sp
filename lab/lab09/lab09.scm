145d7a72e86e10ce0328747aab753d284c52fc8f
miku <jeds9012@gmail.com>
2024-05-20 10:44:35 +0800

    update lab09

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,27 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  'YOUR-CODE-HERE
+  (cond
+      ((< num1 num2) -1)
+      ((< num2 num1) 1)
+      (else 0))
+  )
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  'YOUR-CODE-HERE
+  (lambda (inc) (+ inc num))
+  )
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  'YOUR-CODE-HERE
+  (lambda (x) (f(g x)))
+  )
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  'YOUR-CODE-HERE
+  (if (< n 1)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1))))
+  )
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +33,10 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  'YOUR-CODE-HERE
+  (cond
+      ((zero? a) b)
+      ((zero? b) a)
+      (else (gcd (min a b) (modulo (max a b) (min a b)))))
+  )
