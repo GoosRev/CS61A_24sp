a0a8e077ce9bf15dca34f6395621808e4f4c604c
miku <jeds9012@gmail.com>
2024-05-20 11:08:20 +0800

    update hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,15 +1,27 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  'YOUR-CODE-HERE
+  (cond 
+      ((= exp 0) 1)
+      ((odd? exp) (* base (pow base (- exp 1))))
+      (else (square (pow base (/ exp 2)))))
+  )
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+  'YOUR-CODE-HERE
+  (car (cdr s))
+  )
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+  'YOUR-CODE-HERE
+  (car (cddr s))
+  )
